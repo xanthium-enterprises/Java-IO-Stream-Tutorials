17e393ac975ee97a6e6321d2dba31e606f75c4ce
xanthium-enterprises <contact@xanthium.in>
2020-11-23 18:10:53 +0530

    added comment

---
+++ _3_BinaryStream_RW_DataIOStream/src/_3_BinaryStream_RW_DataIOStream/BinaryFile_ReadWrite.java
@@ -0,0 +1,13 @@
+// Reading and Writing a binary File using 
+package _3_BinaryStream_RW_DataIOStream;
+
+public class BinaryFile_ReadWrite
+{
+
+	public static void main(String[] args) 
+	{
+		hod stub
+
+	}
+
+}
