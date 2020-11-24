d3fb4505dd8e05861eac85f9ccffe2c3f38392bc
xanthium-enterprises <contact@xanthium.in>
2020-11-24 13:24:55 +0530

    added date time api

--- _4_Java8_DateTime_API/src/_4_Java8_DateTime_API/java8_date_time_api.java
+++ _4_Java8_DateTime_API/src/_4_Java8_DateTime_API/java8_date_time_api.java
@@ -6,7 +6,7 @@
 package _4_Java8_DateTime_API;
 
 import java.time.LocalTime;
-
+import java.time.LocalDate;
 
 public class java8_date_time_api 
 {
@@ -14,12 +14,22 @@ public class java8_date_time_api
 	{
 		System.out.println("New java8 Time API\n");
 		
+		// Time Class
+		System.out.println("Time API");
 	    LocalTime TimeVariable = LocalTime.now();
 		System.out.println("LocalTime.now() -> " + TimeVariable);
-		System.out.println("LocalTime.now() -> " + TimeVariable.getHour());
+		System.out.println("Hour 24hr Format -> " + TimeVariable.getHour());
+		System.out.println("         Minutes -> " + TimeVariable.getMinute());
+		System.out.println("         Seconds -> " + TimeVariable.getSecond());
+		System.out.println("    Nano Seconds -> " + TimeVariable.getNano());
 		
+		// Date Class
+		System.out.println("\nDate API");
+		LocalDate DateVariableObj = LocalDate.now();
+		System.out.println("LocalDate.now() -> " + DateVariableObj);
+		System.out.println("           Year -> " + DateVariableObj.getYear());
+		System.out.println("          Month -> " + DateVariableObj.getMonthValue());
+		System.out.println("            Day -> " + DateVariableObj.getDayOfMonth());
 		
-	    
-	    
 	}
 }
