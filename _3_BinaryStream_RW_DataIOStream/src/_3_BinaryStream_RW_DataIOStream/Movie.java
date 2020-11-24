9cee2d5d3c7d587a141580004aef56fba5ab4daf
xanthium-enterprises <contact@xanthium.in>
2020-11-24 12:25:29 +0530

    created java8 date time api

---
+++ _3_BinaryStream_RW_DataIOStream/src/_3_BinaryStream_RW_DataIOStream/Movie.java
@@ -0,0 +1,31 @@
+/**
+ 	Class for storing movie details
+ 	contains
+             Movie Name   -> String 
+             Movie Year   -> Date Variable/String
+             Release date -> Date Variable
+             Movie description - String
+             Director -> String
+             Actors   -> String[]
+             Budget   -> Money Variable
+             
+     @author Rahul
+     @version 0.1
+     
+*/
+
+package _3_BinaryStream_RW_DataIOStream;
+
+public class Movie 
+
+{
+	private String name;
+	private String year;
+	//release_date
+	private String description;
+	private String director;
+	private String[] Actors;
+	
+	
+	
+}
---
+++ _4_Java8_DateTime_API/src/_4_Java8_DateTime_API/java8_date_time_api.java
@@ -0,0 +1,5 @@
+package _4_Java8_DateTime_API;
+
+public class java8_date_time_api {
+
+}
