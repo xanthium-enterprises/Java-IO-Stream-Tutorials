020031299106ed798b209421485bc37a2186a453
xanthium-enterprises <contact@xanthium.in>
2020-11-24 12:30:37 +0530

    Get the current date and time

--- _4_Java8_DateTime_API/src/_4_Java8_DateTime_API/java8_date_time_api.java
+++ _4_Java8_DateTime_API/src/_4_Java8_DateTime_API/java8_date_time_api.java
@@ -5,9 +5,9 @@
 
 package _4_Java8_DateTime_API;
 
+import java.time.LocalDateTime;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.LocalDateTime;
 import java.time.Month;
 
 
@@ -16,5 +16,11 @@ public class java8_date_time_api
 	public static void main(String[] Args)
 	{
 		System.out.println("New java8 Time API");
+		
+		// Get the current date and time
+	    LocalDateTime currentTime = LocalDateTime.now();
+	    System.out.println("Current DateTime: " + currentTime);
+	    
+	    
 	}
 }
