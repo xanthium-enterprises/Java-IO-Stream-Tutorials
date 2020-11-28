f422a5f04c2203d0be88d4707f60a3354ad7c680
xanthium-enterprises <contact@xanthium.in>
2020-11-28 16:14:24 +0530

    .

--- _3_BinaryStream_RW_DataIOStream/src/_3_BinaryStream_RW_DataIOStream/BinaryFile_ReadWrite.java
+++ _3_BinaryStream_RW_DataIOStream/src/_3_BinaryStream_RW_DataIOStream/BinaryFile_ReadWrite.java
@@ -2,6 +2,8 @@
 
 package _3_BinaryStream_RW_DataIOStream;
 
+import java.io.File;
+
 public class BinaryFile_ReadWrite
 {
 
@@ -9,17 +11,55 @@ public class BinaryFile_ReadWrite
 	{
 		Movie[] movies = getMovies();
 		
+		File fhandle = new File("javabinaryfile.bin");
 		
-		for(Movie PM:movies)
+		//create a new binary file in disk
+		try
 		{
-			System.out.println(PM.getName());
-			System.out.println(PM.getYear());
-			System.out.println(PM.getDirector());
-			System.out.println();
+			
+			
+			if(fhandle.createNewFile()) //if file created successfully
+			{
+				System.out.println("File :- " + fhandle.getName() + " created ");
+			}
+				
+			
+		}
+		catch(Exception fileEx)
+		{
+			System.out.println(fileEx.getMessage());
 		}
 		
+		/*
+			for(Movie PM:movies)
+			{
+				System.out.println(PM.getName());
+				System.out.println(PM.getYear());
+				System.out.println(PM.getDirector());
+				System.out.println();
+			}
+		*/
+		
+		//delete the file 
+		try
+		{
+			System.out.println("Press any key to delete " + fhandle.getName());
+			System.in.read(); //wait for key press 
+			
+			if(fhandle.delete())
+			{
+				System.out.println("File :- " + fhandle.getName() + " deleted ");
+			}
+				
+				
+		}
+		catch(Exception fileEx)
+		{
+			System.out.println(fileEx.getMessage());
+		}
 	}//end of main()
 	
+	// to populate the classes with some default values
 	private static Movie[] getMovies()
 	{
 		Movie[] m = new Movie[5];
@@ -30,7 +70,7 @@ public class BinaryFile_ReadWrite
 		m[3] = new Movie("Alien",1979,"Ridley Scott");
 		m[4] = new Movie("Whiplash",2014,"Damien Chazelle");
 		
-		return m;
+		return m;//returns an array of movie objects
 	}
 
 }//end of class
